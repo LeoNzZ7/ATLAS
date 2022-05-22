84a4fb42249f7002f642f7c65cafbbc241982d23
Leonardo Nunes Martinha <Leonardomartinha.dev@gmail.com>
2022-05-22 19:39:28 -0300

    Theme switcher configurado

--- src/components/themeSwitcher/index.tsx
+++ src/components/themeSwitcher/index.tsx
@@ -1,9 +1,19 @@
 import { Power } from 'phosphor-react';
+import { useDispatch } from 'react-redux';
+import { useAppSelector } from '../../redux/hooks/useAppSelector';
+import { setThemeStatus } from '../../redux/reducers/themeReducer';
 import * as c from './styles'
 
 export const ThemeSwitcher = () => {
+    const themeStatus = useAppSelector(state => state.theme.status)
+    const dispatch = useDispatch();
+
+    const handleThemeSwitcher = () => {
+        dispatch(setThemeStatus(themeStatus === 'Dark' ? 'light' : 'Dark'));
+    };
+
     return (
-        <c.Container>
+        <c.Container onClick={handleThemeSwitcher} >
             <c.Handler>
                 <Power size={20} weight="bold" />
             </c.Handler>
